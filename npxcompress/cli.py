4b4ad9c6d928e3f4951b2a67c2fc9881d4757cb1
Vadim Frolov <fralik@gmail.com>
2022-03-26 22:21:53 +0100

    Add decompression capabilities

--- npxcompress/cli.py
+++ npxcompress/cli.py
@@ -1,28 +1,14 @@
-from pathlib import Path
-
 import click
-from mtscomp import compress
+from mtscomp import compress, decompress
 
 from npxcompress.sglx_utils import get_num_saved_channels, get_sample_rate, read_meta
+from npxcompress.utils import delete_files, find_pairs, get_status_and_color, run_and_get_status
 
 
-@click.command()
-@click.argument("root_dir", type=click.Path(exists=True, file_okay=False))
-def main(root_dir: str):
-    """Compress all .bin files in a directory tree
-
-    ROOT_DIR is the path from which the script will start looking for .bin files. The script will traverse all folders
-    starting from ROOT_DIR, find all .bin/.meta files and compress them.
-    """
-    bin_candidates = list(Path(root_dir).rglob("**/*.bin"))
-    metadata_candidates = [x.parent / f"{x.stem}.meta" for x in bin_candidates]
-    bin_files = []
-    metadata_files: list[Path] = []
-    for bin_file, metadata_file in zip(bin_candidates, metadata_candidates):
-        if metadata_file.is_file():
-            bin_files.append(bin_file)
-            metadata_files.append(metadata_file)
-    click.echo(f"Number of .bin/meta pairs: {len(bin_files)}")
+def run_compression(root_dir: str, dry_run: bool):
+    click.echo(f"Starting to compress files found under {root_dir}")
+    bin_files, metadata_files = find_pairs(root_dir)
+    click.echo(f"Number of bin/meta pairs: {len(bin_files)}")
 
     dtype = "int16"
     for bin_file, metadata_file in zip(bin_files, metadata_files):
@@ -31,15 +17,72 @@ def main(root_dir: str):
         n_channels = get_num_saved_channels(meta)
 
         # use default filenames for compressed files
-        compressed_ok = True
-        try:
-            compress(str(bin_file), sample_rate=sample_rate, n_channels=n_channels, dtype=dtype, check_after_compress=True)
-        except:
-            compressed_ok = False
+        compressed_ok = run_and_get_status(
+            compress,
+            dry_run,
+            path=str(bin_file),
+            sample_rate=sample_rate,
+            n_channels=n_channels,
+            dtype=dtype,
+            check_after_compress=True,
+        )
         status = "OK" if compressed_ok else "Failed"
         bg_color = "green" if compressed_ok else "red"
         click.echo(f"Compress {bin_file}" + "... " + click.style(status, bg=bg_color))
 
+        deleted_ok = run_and_get_status(delete_files, dry_run, files=(bin_file,))
+        status, bg_color = get_status_and_color(deleted_ok)
+        click.echo(f"\tRAW files deleted... " + click.style(status, bg=bg_color))
 
-if __name__ == "__main__":
-    main()
+
+def run_decompression(root_dir: str, dry_run: bool):
+    click.echo(f"Starting to decompress files found under {root_dir}")
+    bin_files, metadata_files = find_pairs(root_dir, bin_ext="cbin", meta_ext="ch")
+    click.echo(f"Number of bin/meta pairs: {len(bin_files)}")
+
+    for bin_file, metadata_file in zip(bin_files, metadata_files):
+        output_name = bin_file.parent / f"{bin_file.stem}.bin"
+
+        decompress_ok = run_and_get_status(
+            decompress, dry_run, cdata=str(bin_file), cmeta=str(metadata_file), out=str(output_name), check_after_decompress=True
+        )
+        status, bg_color = get_status_and_color(decompress_ok)
+        click.echo(f"Decompress {bin_file}" + "... " + click.style(status, bg=bg_color))
+
+        deleted_ok = run_and_get_status(delete_files, dry_run, files=(bin_file, metadata_file))
+        status, bg_colors = get_status_and_color(deleted_ok)
+        click.echo(f"\tCompressed files deleted... " + click.style(status, bg=bg_color))
+
+
+@click.command()
+@click.argument("root_dir", type=click.Path(exists=True, file_okay=False))
+@click.option(
+    "-d",
+    "--decompress",
+    default=False,
+    is_flag=True,
+    flag_value=False,
+    help="Whether to decompress files instead of compressing them.",
+)
+@click.option(
+    "--dry-run",
+    default=False,
+    is_flag=True,
+    flag_value=False,
+    help="Perform a dry-run, no compression or decompression will be performed.",
+)
+def main(root_dir: str, decompress: bool, dry_run: bool):
+    """Compress/decompress all bin files in a directory tree
+
+    ROOT_DIR is the path from which the script will start looking for .(c)bin files. The script will traverse all folders
+    starting from ROOT_DIR, find all .(c)bin/.(c)meta files and compress or decompress them.
+    """
+    if decompress:
+        run_decompression(root_dir, dry_run)
+    else:
+        run_compression(root_dir, dry_run)
+
+
+#
+# if __name__ == "__main__":
+#     main()
---
+++ npxcompress/utils.py
@@ -0,0 +1,46 @@
+import random
+from pathlib import Path
+from typing import Callable, Sequence, Tuple
+
+
+def decision(probability: float) -> bool:
+    return random.random() < probability
+
+
+def find_pairs(root_dir: str, bin_ext: str = "bin", meta_ext: str = "meta") -> Tuple[list[Path], list[Path]]:
+    bin_candidates = list(Path(root_dir).rglob(f"**/*.{bin_ext}"))
+    metadata_candidates = [x.parent / f"{x.stem}.{meta_ext}" for x in bin_candidates]
+    bin_files = []
+    metadata_files: list[Path] = []
+    for bin_file, metadata_file in zip(bin_candidates, metadata_candidates):
+        if metadata_file.is_file():
+            bin_files.append(bin_file)
+            metadata_files.append(metadata_file)
+
+    return bin_files, metadata_files
+
+
+def get_status_and_color(flag: bool) -> Tuple[str, str]:
+    status = "OK" if flag else "Failed"
+    bg_color = "green" if flag else "red"
+
+    return status, bg_color
+
+
+def run_and_get_status(func: Callable[..., int], dry_run: bool, **kwargs) -> bool:
+    run_ok = True
+    try:
+        if dry_run:
+            shall_fail = decision(0.8)
+            if shall_fail:
+                raise Exception("fail")
+        else:
+            func(**kwargs)
+    except:
+        run_ok = False
+    return run_ok
+
+
+def delete_files(files: Sequence[Path]):
+    for file in files:
+        file.unlink()
