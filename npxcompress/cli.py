fe8615ad78badae5ff35e5201e0c926a47d1bfd3
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2024-11-07 11:23:11 +0100

    add --keep-original flag
    Force source files to not be deleted regardless of de/compression success

--- npxcompress/cli.py
+++ npxcompress/cli.py
@@ -5,7 +5,7 @@ from npxcompress.sglx_utils import get_num_saved_channels, get_sample_rate, read
 from npxcompress.utils import delete_files, find_pairs, get_status_and_color, run_and_get_status
 
 
-def run_compression(root_dir: str, dry_run: bool):
+def run_compression(root_dir: str, dry_run: bool, keep_original: bool):
     click.echo(f"Starting to compress files found under {root_dir}")
     bin_files, metadata_files = find_pairs(root_dir)
     click.echo(f"Number of bin/meta pairs: {len(bin_files)}")
@@ -28,15 +28,16 @@ def run_compression(root_dir: str, dry_run: bool):
         )
         status, bg_color = get_status_and_color(compressed_ok, dry_run)
         click.echo(f"Compress {bin_file}" + "... " + click.style(status, bg=bg_color))
-        if compressed_ok and not dry_run:
+        if compressed_ok and not dry_run and not keep_original:
             deleted_ok = run_and_get_status(delete_files, dry_run, files=(bin_file,))
             status, bg_color = get_status_and_color(deleted_ok)
-            click.echo(f"\tRAW files deleted... " + click.style(status, bg=bg_color))
         else:
-            click.echo("\tRAW files not deleted")
+            status, bg_color = get_status_and_color(False, True)
+        click.echo(f"\tRAW files deleted... " + click.style(status, bg=bg_color))
+    return
 
 
-def run_decompression(root_dir: str, dry_run: bool):
+def run_decompression(root_dir: str, dry_run: bool, keep_original: bool):
     click.echo(f"Starting to decompress files found under {root_dir}")
     bin_files, metadata_files = find_pairs(root_dir, bin_ext="cbin", meta_ext="ch")
     click.echo(f"Number of bin/meta pairs: {len(bin_files)}")
@@ -50,12 +51,13 @@ def run_decompression(root_dir: str, dry_run: bool):
         status, bg_color = get_status_and_color(decompressed_ok, dry_run)
         click.echo(f"Decompress {bin_file}" + "... " + click.style(status, bg=bg_color))
 
-        if decompressed_ok and not dry_run:
+        if decompressed_ok and not dry_run and not keep_original:
             deleted_ok = run_and_get_status(delete_files, dry_run, files=(bin_file, metadata_file))
             status, bg_colors = get_status_and_color(deleted_ok)
-            click.echo(f"\tCompressed files deleted... " + click.style(status, bg=bg_color))
         else:
-            click.echo("\tCompressed files not deleted")
+            status, bg_color = get_status_and_color(False, True)
+        click.echo(f"\tCompressed files deleted... " + click.style(status, bg=bg_color))
+    return
 
 
 @click.command()
@@ -73,16 +75,23 @@ def run_decompression(root_dir: str, dry_run: bool):
     is_flag=True,
     help="Perform a dry-run, no compression or decompression will be performed.",
 )
-def main(root_dir: str, decompress: bool, dry_run: bool):
+@click.option(
+    "-k",
+    "--keep-original",
+    default=False,
+    is_flag=True,
+    help="Keep the source files after compression/decompression (otherwise deleted by default).",
+)
+def main(root_dir: str, decompress: bool, dry_run: bool, keep_original: bool):
     """Compress/decompress all bin files in a directory tree
 
     ROOT_DIR is the path from which the script will start looking for .(c)bin files. The script will traverse all folders
     starting from ROOT_DIR, find all .(c)bin/.(c)meta files and compress or decompress them.
     """
     if decompress:
-        run_decompression(root_dir, dry_run)
+        run_decompression(root_dir, dry_run, keep_original)
     else:
-        run_compression(root_dir, dry_run)
+        run_compression(root_dir, dry_run, keep_original)
 
 
 if __name__ == "__main__":
